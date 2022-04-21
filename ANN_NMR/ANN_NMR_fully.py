dff60c1f1275cf74f5c4a2acc0b4283590a5c6b5
Peter Sagmeister <91178324+SagmeisterPeter@users.noreply.github.com>
2022-04-21 12:46:13 +0200

    Update ANN_NMR_fully.py

--- ANN_NMR/ANN_NMR_fully.py
+++ ANN_NMR/ANN_NMR_fully.py
@@ -22,7 +22,7 @@ from tensorflow.keras.layers import Dense, Input, Flatten, LocallyConnected1D,Co
 from tensorflow.keras.optimizers import Adam
 maxnorm = max_norm(max_value=2, axis=0)
 #%%import training data and validation data
-path1 = ''
+path1 = ''                              # insert path where the training data and validation data is saved
 
 simulated_set = np.load(path1+'NMR_simulated_set.npy')
 experimental_mixtures_set = np.load(path1+'NMR_experimental_mixtures_set.npy')
@@ -33,17 +33,17 @@ train_set = np.concatenate((experimental_mixtures_set, simulated_set), axis=0)
 np.random.shuffle(dynamic_set )
 np.random.shuffle(train_set)
 
-train_spec = train_set[:,3:]
-train_conc = train_set[:,0:3]
-val_spec = dynamic_set [:,3:]
-val_conc = dynamic_set [:,0:3]
+train_spec = train_set[:,3:]            #assign spectra of the training set to a variable 
+train_conc = train_set[:,0:3]           #assign concentrations of the training set to a variable 
+val_spec = dynamic_set [:,3:]           #assign spectra of the validation set to a variable 
+val_conc = dynamic_set [:,0:3]          #assign concentrations of the validation set to a variable 
 
 #%% normalization of the NMR spectrum of training and validation set
-train_spec_norm = np.empty(np.shape(train_spec)) 
+train_spec_norm = np.empty(np.shape(train_spec))                    # normalization of the training spectra 
 for i in range(len(train_spec)):
     train_spec_norm[i] = train_spec[i] / 1000
 
-val_spec_norm = np.empty(np.shape(val_spec)) 
+val_spec_norm = np.empty(np.shape(val_spec))                        # normalization of the validation spectra
 for i in range(len(val_spec)):
     val_spec_norm[i] = val_spec[i] / 1000
 
@@ -52,13 +52,13 @@ val_spec = val_spec_norm
 
 #%%  normalization of concentration of the training and validation set
 train_conc_norm = np.empty(np.shape(train_conc)) 
-for i in range(len(train_conc)):
+for i in range(len(train_conc)):                                # normalization of the training concentrations
         train_conc_norm[i,0] = train_conc[i,0] / 0.28
         train_conc_norm[i,1] = train_conc[i,1] / 0.1
         train_conc_norm[i,2] = train_conc[i,2] / 0.28
     
 val_conc_norm  = np.empty(np.shape(val_conc)) 
-for i in range(len(val_conc)):
+for i in range(len(val_conc)):                                  # normalization of the validation concentrations
     val_conc_norm[i,0] = val_conc [i,0] / 0.28
     val_conc_norm[i,1] = val_conc [i,1] / 0.1
     val_conc_norm[i,2] = val_conc [i,2] / 0.28
@@ -70,14 +70,14 @@ val_conc = val_conc_norm
 xp = np.linspace(7, 9, 1148)    
 x = np.linspace(7,9 ,600)
 
-train_spec_red = np.zeros((len(train_spec),600))
+train_spec_red = np.zeros((len(train_spec),600))                # reduce the data points of each NMR spectra
 for i in range(0,len(train_spec)):  
     interpol = np.interp(x, xp, train_spec[i], left=None, right=None, period=None)
     train_spec_red[i] = interpol
 
 train_spec = train_spec_red
 
-val_spec_red = np.zeros((len(val_spec),600))
+val_spec_red = np.zeros((len(val_spec),600))                   # reduce the data points of each NMR spectra
 for i in range(0,len(val_spec)):  
     interpol = np.interp(x, xp, val_spec[i], left=None, right=None, period=None)
     val_spec_red[i] = interpol
@@ -85,17 +85,17 @@ for i in range(0,len(val_spec)):
 val_spec= val_spec_red
 
 #%% define the model and define the architecture of the ANN
-path2 = ''
-model_path_name = 'model_1.hdf5'  
+path2 = ''                                      # insert path where the model should be saved
+model_path_name = 'model_1.hdf5'                # insert model name  
 
 # Architecture of the NMR model
-visible = Input(shape=(600,))
-hidden11 = Dense(600,activation='relu')(visible)
-hidden12 = Dense(243, activation='relu')(hidden11)
-hidden13 = Dense(81, activation='relu')(hidden12)
-hidden14 = Dense(9, activation='relu')(hidden13)
-output = Dense(3, activation='relu')(hidden14)
-model = Model(inputs=visible, outputs=output)
+visible = Input(shape=(600,))                       # input layer (spectrum size)
+hidden11 = Dense(600,activation='relu')(visible)    # dense layer 1
+hidden12 = Dense(243, activation='relu')(hidden11)  # dense layer 2
+hidden13 = Dense(81, activation='relu')(hidden12)   # dense layer 3
+hidden14 = Dense(9, activation='relu')(hidden13)    # dense layer 4
+output = Dense(3, activation='relu')(hidden14)      # dense layer 5
+model = Model(inputs=visible, outputs=output)       # output layer
 # summarize model
 print(model.summary())
 # compile the fully dense ANN model
